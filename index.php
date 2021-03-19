b65b0194620fc875353d6d586f87cde959b7b992
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2021-03-19 17:43:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,50 @@
 <!DOCTYPE html>
-
 <html>
-
     <head>
-
-        <title></title>
-
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+   <style>
+       body {
+           padding:0;
+           margin:0;
+           background-color: aliceblue;
+       }
+div {
+    position: fixed;
+    width:80%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding:15px 10px;
+    background:white;
+    border-radius:8px;
+    text-align: justify;
+    text-justify: inter-word;
+    box-shadow: 0 3px 8px 0 rgba(0,0,255,.2);
+    line-height: 25px;
+}
 
-        <style>
+h1 {
+    font-size:50px;
+    margin:5px;
+    color:#ff4500;
+}
 
-            body {
+p {
+    text-indent: 50px;
+}
 
-                width:100%;
-
-                height:100%;
-
-                background: rgba(0, 0, 0, 0) url(./maintenance.png) fixed no-repeat center center / 80% auto;
-
-            }
-
-        </style>
-
+</style>
     </head>
-
     <body>
-
+       
+        
+            <div>
+                <center>
+                    <h1>&#9888;</h1>
+                </center>
+                <h4>Hey Student!</h4>
+                <p>Sorry for the inconvenience caused to you. unfortunately, the App Server have some serious issues. So, we working to fix those things as soon as possible. Please wait with some patience, we will bring back the app to as usual condition before tomorrow morning. We hope you understand us. <br><br> <b style="float:right"> - App Developer</b></p>
+            </div>
+            
     </body>
-
 </html>
